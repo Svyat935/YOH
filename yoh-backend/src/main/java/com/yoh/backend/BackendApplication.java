a53f805837a50b262bc5557ef52eb11a8839c11c
LegendaRepa <sirokko_dark@mail.ru>
2022-04-09 22:43:29 +0300

    Admin creation in initialization of the project was added

--- yoh-backend/src/main/java/com/yoh/backend/BackendApplication.java
+++ yoh-backend/src/main/java/com/yoh/backend/BackendApplication.java
@@ -1,14 +1,40 @@
 package com.yoh.backend;
 
+import com.yoh.backend.entity.Admin;
+import com.yoh.backend.entity.User;
+import com.yoh.backend.repository.AdminRepository;
+import com.yoh.backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
 public class BackendApplication {
 
+	@Value("${INIT_ADMIN_LOGIN}")
+	private String login;
+
+	@Value("${INIT_ADMIN_PASSWORD}")
+	private String password;
+
+	@Value("${INIT_ADMIN_EMAIL}")
+	private String email;
+
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner init(AdminRepository adminRepository, UserRepository userRepository) {
+		return (args) -> {
+			User user = new User(this.login, this.email, this.password, 0);
+			Admin admin = new Admin(user);
+			userRepository.createUser(user);
+			adminRepository.createAdmin(admin);
+		};
 	}
 
 }
--- yoh-backend/src/main/java/com/yoh/backend/entity/User.java
+++ yoh-backend/src/main/java/com/yoh/backend/entity/User.java
@@ -14,6 +14,13 @@ public class User {
         this.password = password;
     }
 
+    public User(String login, String email, String password, Integer role) {
+        this.login = login;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
 
     @Id
     @GeneratedValue
