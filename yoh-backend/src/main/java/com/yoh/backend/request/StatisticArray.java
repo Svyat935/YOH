34983e5472ac686426a927e74387193f437e3b3a
LegendaRepa <sirokko_dark@mail.ru>
2022-04-19 23:58:31 +0300

    sueta

--- yoh-backend/src/main/java/com/yoh/backend/request/StatisticArray.java
+++ yoh-backend/src/main/java/com/yoh/backend/request/StatisticArray.java
@@ -1,15 +1,17 @@
 package com.yoh.backend.request;
 
+import com.github.cliftonlabs.json_simple.JsonObject;
+
 import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 
 public class StatisticArray {
     @NotNull
-    private ArrayList<GameStatisticToSend> records;
+    private ArrayList<JsonObject> records;
 
-    public ArrayList<GameStatisticToSend> getRecords() {
+    public ArrayList<JsonObject> getRecords() {
         return records;
     }
 
-    public void setGames_id(ArrayList<GameStatisticToSend> records) {this.records = records;}
+    public void setGames_id(ArrayList<JsonObject> records) {this.records = records;}
 }
