bd6dfda59eebfefeb7ce1842487d1b8629236a29
Svyat935 <k.svyat395@gmail.com>
2022-05-20 22:03:25 +0300

    fix

--- yoh-frontend/src/pages/users/admin/pages/users/CUsersAdmin.js
+++ yoh-frontend/src/pages/users/admin/pages/users/CUsersAdmin.js
@@ -9,7 +9,9 @@ export function CUsersAdmin() {
     const [_, rerun] = useState(new class{});
 
     const requestUsers = async () => {
-        return await fetch("/admins/users/all", {
+        return await fetch("/admins/users/all?" +
+            "start=" + encodeURIComponent(0) + "&" +
+            "limit=" + encodeURIComponent(100), {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
