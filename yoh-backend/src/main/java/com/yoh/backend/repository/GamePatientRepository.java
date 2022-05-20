13140fb4e9e3d9b23b00e4adccbc46fcf7a0a6e4
Svyat935 <k.svyat395@gmail.com>
2022-05-20 21:36:57 +0300

    fix

--- yoh-backend/src/main/java/com/yoh/backend/repository/GamePatientRepository.java
+++ yoh-backend/src/main/java/com/yoh/backend/repository/GamePatientRepository.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import java.util.List;
 import java.util.UUID;
 
+@Repository
+@Transactional
 public class GamePatientRepository {
 
     @Autowired
