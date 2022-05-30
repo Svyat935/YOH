e559ace2736014d81a7683caf39e620736de7c3c
Svyat935 <k.svyat395@gmail.com>
2022-05-31 02:16:51 +0300

    fix

--- yoh-frontend/src/pages/users/tutor/pages/dashboard/charts/TransitTime.js
+++ yoh-frontend/src/pages/users/tutor/pages/dashboard/charts/TransitTime.js
@@ -87,6 +87,7 @@ export class TransitTime extends Component {
     componentDidUpdate(prevProps, prevState, snapshot){
         let title = prevProps.title !== null ? this.toDate(prevProps.title) : "Игра в процессе";
 
+        console.log(this.data, prevProps.data);
         if (this.data !== prevProps.data){
             this.setState(prevProps.data);
             this.setState(
