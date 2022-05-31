367f0f9e36a3347319d222302c26218d04604e49
Svyat935 <k.svyat395@gmail.com>
2022-05-31 11:33:08 +0300

    fix

--- yoh-frontend/src/pages/users/tutor/pages/game/VGame.js
+++ yoh-frontend/src/pages/users/tutor/pages/game/VGame.js
@@ -7,35 +7,26 @@ import {LoadPage} from "../../../../../components/loadpage/LoadPage";
 
 export function VGame() {
     let context = useContext(UserContext);
-    const [view, setView] = useState(null);
     const [load, setLoad] = useState(true);
-
-    useEffect(() => {
-        if (context.token){
-            setView(
-                <div style={
-                    {
-                        display: "flex",
-                        justifyContent: "center"
-                    }
-                }>
-                    <LoadPage
-                        status={load}
-                    >
-                        <iframe
-                            src={context.info.url}
-                            style={{width: "95vw", height: "82vh"}}
-                            onLoad={() => setLoad(false)}
-                        />
-                    </LoadPage>
-                </div>
-            )
-        }
-    }, [context])
 
     return (
         <Back navPanel={<TutorNav context={context}/>}>
-            {view}
+            <div style={
+                {
+                    display: "flex",
+                    justifyContent: "center"
+                }
+            }>
+                <LoadPage
+                    status={load}
+                >
+                    <iframe
+                        src={context.info.url}
+                        style={{width: "95vw", height: "82vh"}}
+                        onLoad={() => setLoad(false)}
+                    />
+                </LoadPage>
+            </div>
         </Back>
     )
 }
