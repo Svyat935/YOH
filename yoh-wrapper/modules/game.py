20a315fbe0929d82b896406fa37bd0a612e4c1d7
nl.lukyanov <nl.lukyanov@tensor.ru>
2022-06-10 09:12:46 +0300

    fix deleting endgame cookie

--- yoh-wrapper/modules/game.py
+++ yoh-wrapper/modules/game.py
@@ -50,7 +50,8 @@ def game_route(game):
     if str(use_statistics).lower() == 'false':
         url = f'/games/{game}/?token={request.args["token"]}'
         resp = make_response(render_template_wo_statistics(url))
-        resp.set_cookie('EndGame', '', expires=0, samesite=None, secure=True)
+        # resp.set_cookie('EndGame', '', expires=0, samesite=None, secure=True)
+        resp.headers.add('Set-Cookie', 'EndGame=false; SameSite=None; Secure')
         return resp
 
     try:
@@ -61,6 +62,7 @@ def game_route(game):
     session['current_game'] = game
 
     resp = make_response(template)
-    resp.set_cookie('EndGame', '', expires=0, samesite=None, secure=True)
+    # resp.set_cookie('EndGame', '', expires=0, samesite=None, secure=True)
+    resp.headers.add('Set-Cookie', 'EndGame=false; SameSite=None; Secure')
 
     return resp
